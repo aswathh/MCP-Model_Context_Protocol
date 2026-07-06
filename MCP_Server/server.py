606bdf4542f47ac42f8090c60eb9c6a3a8a46449
aswathh <aswathkumar2001@gmail.com>
2026-07-06 10:17:06 +0530

    list files tool created

--- MCP_Server/server.py
+++ MCP_Server/server.py
@@ -8,6 +8,22 @@ logging.basicConfig(level=logging.DEBUG,format='%(asctime)s-%(levelname)s-%(mess
 logger = logging.getLogger(__name__)
 
 
+@app.tool(description="List files and folder under the directory Path")
+def list_files(path:str):
+    try:
+        return os.listdir(path)
+    except Exception as e:
+        logger.exception("Failed to list the files")
+        return f"Error listing files:{e}"
+
+@app.tool(Description="Read and return the text content of the file")
+def read_file(path:str):
+    try:
+        with open(path,"r",encoding="utf-8") as f:
+            return f.read()
+    except Exception as e:
+        logger.exception("Failed to read")
+        return f"Error reading file:{e}"
 
 
 if __name__ == "__main__":
