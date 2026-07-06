6975d3193ef7cc09ff205105ff401e03e9c4ffdd
aswathh <aswathkumar2001@gmail.com>
2026-07-06 10:08:51 +0530

    logger added

--- MCP_Server/server.py
+++ MCP_Server/server.py
@@ -4,6 +4,9 @@ import logging
 
 app = FastMCP("file_system")
 
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s-%(levelname)s-%(message)s')
+logger = logging.getLogger(__name__)
+
 
 
 
