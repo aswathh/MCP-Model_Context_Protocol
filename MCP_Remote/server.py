5dff89c7cc255d78db182723dc0e3ed9458fb1f7
aswathh <aswathkumar2001@gmail.com>
2026-07-07 10:36:09 +0530

    tools created in mcp server

--- MCP_Remote/server.py
+++ MCP_Remote/server.py
@@ -11,8 +11,32 @@ mcp = FastMCP("filesystem for remote")
 BASE_DIR =Path(__file__).parent
 DATA_DIR = BASE_DIR/"data"
 
-@mcp.tool(dascription:)
-def list_files(path:str):
+@mcp.tool(description="list all the files")
+def list_files():
+    logging.info("listing files")
+    files = os.listdir(DATA_DIR)
+    return files
+
+@mcp.tool(description="Read the file")
+def read_file(filename:str):
+    logging.info(f"reading this file {filename}")
+
+    path = DATA_DIR/filename
+
+    if not path.exists():
+        return "File not found"
+    with open(path,"r",encoding="utf-8")as f:
+        return f.read()
+
+@mcp.tool(description="write a new content in the file")
+def write_file(filename:str,content:str):
+    logging.info(f"writing the new content in this file {filename}")
+    path = DATA_DIR/filename
+    if not path.exists():
+        return "file not found"
+    with open(path,"w",encoding="utf-8")as f:
+        return f.write(content)
+
 
 
 
